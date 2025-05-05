7846c55bbf7d694109ac7b18a1133b2767ef288a
Nils <spuggel0@gmail.com>
2025-05-05 20:30:38 +0200

    Map manager things

---
+++ scripts/map-manager/floor_placeholder_tile.gd
@@ -0,0 +1,18 @@
+# TODO: Replace this script with manually tiled rooms or single large sprites
+extends Node2D
+
+
+var icon_svg: CompressedTexture2D = preload("res://icon.svg")
+
+
+func _ready() -> void:
+	# Iterate through all collision in the scene and place an icon.svg matching each of it
+	for body in get_children():
+		for shape in body.get_children():
+			var new_sprite: Sprite2D = Sprite2D.new()
+
+			new_sprite.texture = icon_svg
+			new_sprite.centered = true
+			new_sprite.scale = shape.shape.size / icon_svg.get_size()
+
+			shape.add_child(new_sprite)
--- scripts/map-manager/room.gd
+++ scripts/map-manager/room.gd
@@ -1,11 +1,61 @@
 extends Node2D
 
+var wall_types_h: Dictionary = {
+	"closed": preload("res://scenes/map-manager/walls/horizontal_closed.tscn"),
+	"open": preload("res://scenes/map-manager/walls/horizontal_open.tscn")
+}
 
-# Called when the node enters the scene tree for the first time.
+var wall_types_v: Dictionary = {
+	"closed": preload("res://scenes/map-manager/walls/vertical_closed.tscn"),
+	"open": preload("res://scenes/map-manager/walls/vertical_open.tscn")
+}
+
+var floors: Array[PackedScene]
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var floor_dir: DirAccess = DirAccess.open("res://scenes/map-manager/floors")
+	floor_dir.list_dir_begin()
+
+	var floor_scene: String = floor_dir.get_next()
+	while floor_scene != "":
+		print("here")
+		floors.append(load("res://scenes/map-manager/floors/" + floor_scene))
+
+		floor_scene = floor_dir.get_next()
+
+	floor_dir.list_dir_end()
+
+	randomize()
+	generate_random_room()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func generate_random_room() -> void:
+	# Destroy any current walls
+	for child in get_children():
+		if child is Control: continue
+
+		child.queue_free()
+
+	# Place randomly generated walls on all sides
+	var wall_instance = wall_types_h[wall_types_h.keys().pick_random()].instantiate()
+	add_child(wall_instance)
+	move_child(wall_instance, 0)
+
+	wall_instance = wall_types_h[wall_types_h.keys().pick_random()].instantiate()
+	add_child(wall_instance)
+	wall_instance.set_global_position(Vector2(0, get_viewport_rect().size.y - 32)) # Move to bottom of screen excluding wall width
+	move_child(wall_instance, 0)
+
+	wall_instance = wall_types_v[wall_types_v.keys().pick_random()].instantiate()
+	add_child(wall_instance)
+	move_child(wall_instance, 0)
+
+	wall_instance = wall_types_v[wall_types_v.keys().pick_random()].instantiate()
+	add_child(wall_instance)
+	wall_instance.set_global_position(Vector2(get_viewport_rect().size.x - 32, 0))
+	move_child(wall_instance, 0)
+
+	# Select a random floor
+	var floor_instance = floors.pick_random().instantiate()
+	add_child(floor_instance)
+	move_child(floor_instance, 0)
